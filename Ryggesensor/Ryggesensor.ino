913e07352690174df8055841aa6e8315161c5430
Sigve <sigve.fast@novacare.no>
2016-01-31 23:35:57 +0100

    Added second distance sensor

--- Ryggesensor/Ryggesensor.ino
+++ Ryggesensor/Ryggesensor.ino
@@ -1,15 +1,24 @@
 #include "Lib/NewPing/NewPing.h"
 #include "Lib/TonePlayer/TonePlayer.h"
 
+enum DistanceSensor {
+  A,
+  B
+};
+
 #define piezoPin 9
 #define NOTE_F3  175
 
-#define TRIGGER_PIN  12
-#define ECHO_PIN     11
+#define TRIGGER_PIN_A  12
+#define ECHO_PIN_A     11
+#define TRIGGER_PIN_B  7
+#define ECHO_PIN_B     6
 #define MAX_DISTANCE 100
 
 TonePlayer tone1 (TCCR1A, TCCR1B, OCR1AH, OCR1AL, TCNT1H, TCNT1L);  // pin D9 (Uno)
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+NewPing sonarA(TRIGGER_PIN_A, ECHO_PIN_A, MAX_DISTANCE);
+NewPing sonarB(TRIGGER_PIN_B, ECHO_PIN_B, MAX_DISTANCE);
 
 
 void setup() {
@@ -18,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-  int distance = getDistance();
+  int distance = getShortestDistance();
   int noteDuration = distance * 10;
   playNote(piezoPin, NOTE_F3, noteDuration);
   delay(100);
@@ -34,9 +43,24 @@ void playNote(int buzzerPin, int note, int duration) {
 }
 
 
-long getDistance() {
+long getShortestDistance() {
   delay(50);
-  int uS = sonar.ping();
+  int uS_A = sonarA.ping();
+  delay(50);
+  int uS_B = sonarB.ping();
+  //long uS_A = 10000000;
+
+  int uS = 0;
+  
+  if(uS_A < uS_B) {
+    Serial.println("A is shorter");
+    uS = uS_A;
+  }
+  else {
+    Serial.println("B is shorter");
+    uS = uS_B;
+  }
+  
   Serial.print("Ping: ");
   Serial.print(uS / US_ROUNDTRIP_CM);
   Serial.println("cm");
